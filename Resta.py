8c0aef3a9f8400dd43a4a2a135e5c1498e527f4f
Abig4il-byte <biasoir1@gmail.com>
2025-09-25 08:03:25 -0600

    Sum_and_subtraction
    You can use the basic functions of the calculator, accessible interface, allowing them to solve calculations quickly and easy.

---
+++ Resta.py
@@ -0,0 +1,4 @@
+numero1=float(input("Ingresa una cantidad al primer numero: "))
+numero2=float(input("Ingresa una cantidad al segundo numero: "))
+resta= numero1 - numero2
+print("El resultado de ", numero1, "-", numero2, "es",resta)
